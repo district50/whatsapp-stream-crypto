bc1c222212dfca946d73eb0a81035f1f32792e2b
Бодин Сергей <s.bodin@walnut.team>
2026-09-10 15:19:02 +0300

    fix

--- app/Transformers/Data/PriceMatrixTransformer.php
+++ app/Transformers/Data/PriceMatrixTransformer.php
@@ -1,28 +1,27 @@
 <?php
 
-namespace App\Transformers\Data;
+    namespace App\Transformers\Data;
 
-use App\Models\PriceMatrix;
-use League\Fractal\TransformerAbstract;
+    use App\Models\PriceMatrix;
+    use League\Fractal\TransformerAbstract;
 
-class PriceMatrixTransformer extends TransformerAbstract {
-    protected array $defaultIncludes = [
+    class PriceMatrixTransformer extends TransformerAbstract {
+        protected array $defaultIncludes = [
 
-    ];
-
-    protected array $availableIncludes = [
-
-    ];
-
-    public function transform(PriceMatrix $model): array {
-        return [
-            'id' => $model->id,
-            'product_type' => $model->product_type,
-            'calculate_type' => $model->calculate_type,
-            'width_value' => $model->width_value,
-            'height_value' => $model->height_value,
-            'price' => $model->price,
         ];
-    }
 
-}
+        protected array $availableIncludes = [
+
+        ];
+
+        public function transform( PriceMatrix $model ): array {
+            return [
+                'id' => $model->id,
+                'product_type' => $model->product_type,
+                'calculate_type' => $model->calculate_type,
+                'width_value' => $model->width_value,
+                'height_value' => $model->height_value,
+                'price' => $model->price,
+            ];
+        }
+    }
